8df5eb624eef90fc2b42e86ea2b48a27b00c7f54
Xurik <Xurshidbekruziyev05@gmail.com>
2023-11-24 15:02:18 +0500

    nothing changed much

--- guess.py
+++ guess.py
@@ -4,12 +4,12 @@ import random
 
 n = random.randint(0, 10)
 
-guess = int(input("guess a number between 0 and 10 bruv: ")
+guess = int(input("PLEASE, guess a number between 0 and 10 bruv: ")
 
 
 While == True
     if n == guess:
 
-        print("Topdingiz")
+        print("Topdingiz !!!!!")
         break
 
